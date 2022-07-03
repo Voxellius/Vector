1a23158da007a73e9953dfcedda73abb97af0f04
James-Livesey <jamesliveg@gmail.com>
2022-07-03 12:22:16 +0100

    Add login window icon

--- src/LoginDialog.cpp
+++ src/LoginDialog.cpp
@@ -2,6 +2,7 @@
 #include <LibGUI/Window.h>
 #include <LibGUI/Widget.h>
 #include <LibGUI/ImageWidget.h>
+#include <LibGfx/Bitmap.h>
 
 #include "LoginDialog.h"
 #include "../LoginDialogGML.h"
@@ -12,10 +13,12 @@ LoginDialog::LoginDialog() {
     widget->load_from_gml(login_dialog_gml);
 
     auto& icon = *widget->find_descendant_of_type_named<GUI::ImageWidget>("icon");
+    auto window_icon_bitmap = MUST(Gfx::Bitmap::try_load_from_file("/usr/local/share/vector/icon_s.png"));
 
     icon.load_from_file("/res/icons/32x32/key.png");
 
     set_title("Log into Vector");
+    set_icon(window_icon_bitmap);
     set_resizable(false);
     resize(450, 180);
 }
